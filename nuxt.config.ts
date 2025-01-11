89fbca482794b78e340202994cc73c4de2d759b9
ydah <t.yudai92@gmail.com>
2025-01-11 10:16:06 +0900

    Create LP

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,11 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   build: {
     transpile: ['vuetify'],
   },
+
   modules: [
     (_options, nuxt) => {
       nuxt.hooks.hook('vite:extendConfig', (config) => {
@@ -15,6 +17,7 @@ export default defineNuxtConfig({
     },
     //...
   ],
+
   vite: {
     vue: {
       template: {
@@ -22,4 +25,39 @@ export default defineNuxtConfig({
       },
     },
   },
+  app: {
+    head: {
+      title: '関西Ruby会議08',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: '関西Ruby会議は、関西で定期的に開催しているRubyプログラミング言語に関するカンファレンスです。Rubyに関する知見・技術を共有し、またRubyistの交流の場として、皆様に楽しんでいただけるカンファレンスを目指しています。' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:site_name', content: '関西Ruby会議08' },
+        // { property: 'og:image', content: 'https://rubykansai.github.io/kansai08/ogp.png' },
+        { property: 'og:description', content: '関西Ruby会議は、関西で定期的に開催しているRubyプログラミング言語に関するカンファレンスです。Rubyに関する知見・技術を共有し、またRubyistの交流の場として、皆様に楽しんでいただけるカンファレンスを目指しています。' },
+        { property: 'og:url', content: 'https://regional.rubykaigi.org/kansai08/' },
+        { property: 'og:title', content: '関西Ruby会議08' },
+        { name: 'twitter:title', content: '関西Ruby会議08' },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:site', content: '@rubykansai' },
+        // { name: 'twitter:image', content: 'https://rubykansai.github.io/kansai08/ogp.png' },
+        { name: 'twitter:description', content: '関西Ruby会議は、関西で定期的に開催しているRubyプログラミング言語に関するカンファレンスです。Rubyに関する知見・技術を共有し、またRubyistの交流の場として、皆様に楽しんでいただけるカンファレンスを目指しています。' },
+      ],
+      link: [
+        // { rel: 'icon', type: 'image/x-icon', href: '/kansai08/favicon.ico' },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Yuji+Syuku&display=swap',
+          crossorigin: '',
+        },
+      ],
+      htmlAttrs: {
+        lang: 'ja',
+        prefix: 'og: http://ogp.me/ns#',
+      },
+    },
+    baseURL: '/kansai08/',
+  },
+  compatibilityDate: '2025-01-10',
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,38 @@
 <template>
-  <v-app-bar :elevation="2">
-    <v-app-bar-title>関西Ruby会議08</v-app-bar-title>
-  </v-app-bar>
+  <div class="fill-height">
+    <v-row align-content="center fill-height">
+      <v-col align="center">
+        <v-card
+          :variant="flat"
+          min-width="100vw"
+          class="text-center py-10"
+        >
+          <v-card-item class="my-10">
+            <div>
+              <div class="text-h2 mb-15">
+                関西Ruby会議08 / KansaiRubyKaigi08
+              </div>
+              <div class="text-h4 mb-5">
+                日時:2025年6月28日(土)
+              </div>
+              <div class="text-h4 mb-10">場所:<a href="https://www.kamogawa-odori.com/kaburenjou/">先斗町歌舞練場</a></div>
+              <div class="mb-10">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3267.944713344888!2d135.7686033758716!3d35.00808906665747!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x600108eb55955555%3A0x72d899efcbcaca3f!2z5YWI5paX55S6IOatjOiInue3tOWgtA!5e0!3m2!1sja!2sjp!4v1736554814030!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+              </div>
+            </div>
+            <a href="https://twitter.com/rubykansai">
+              続報をお待ち下さい
+            </a>
+          </v-card-item>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
+
+<style scoped>
+* {
+  font-family: "Yuji Syuku", serif;
+  font-style: normal;
+}
+</style>
